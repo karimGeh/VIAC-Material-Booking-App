78f1281f736fd3586026d7728a66e2a5ff5fccca
karimGeh <karim2jihad@gmail.com>
2023-05-18 13:26:02 +0100

    update some text styles

--- src/mobile_app/lib/src/styles/textStyles.dart
+++ src/mobile_app/lib/src/styles/textStyles.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:mobile_app/src/styles/colors.dart';
 
 class CustomTextStyles {
-  static h1({Color? color}) {
+  static h1({
+    Color? color,
+    FontWeight? fontWeight = FontWeight.normal,
+    TextAlign? textAlign = TextAlign.left,
+  }) {
     return TextStyle(
       // 36 bold inter
       fontSize: 36.0,
-      fontWeight: FontWeight.bold,
+      fontWeight: fontWeight,
       color: color ?? CustomColors.black,
     );
   }
@@ -19,7 +23,7 @@ class CustomTextStyles {
     return TextStyle(
       // 24 bold inter
       fontSize: 24.0,
-      fontWeight: FontWeight.bold,
+      fontWeight: fontWeight,
       color: color,
     );
   }
@@ -45,7 +49,7 @@ class CustomTextStyles {
     return TextStyle(
       // 14 bold inter
       fontSize: 18.0,
-      fontWeight: FontWeight.bold,
+      fontWeight: fontWeight,
       color: color,
     );
   }
