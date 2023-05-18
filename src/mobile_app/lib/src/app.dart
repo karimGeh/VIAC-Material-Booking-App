4015f7d89152e5e1327f256367db05c478a6c8b8
karimGeh <karim2jihad@gmail.com>
2023-05-18 13:25:49 +0100

    integrate router observer with main and app

--- src/mobile_app/lib/src/app.dart
+++ src/mobile_app/lib/src/app.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/src/router/NavigatorObserver.dart';
 import 'package:mobile_app/src/router/OnGenerateRoute.dart';
 import 'package:mobile_app/src/router/routes.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  App({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Material booking app',
       initialRoute: Routes.initialRoute,
       onGenerateRoute: onGenerateRoute,
+      navigatorObservers: [
+        MainRouteObserver,
+      ],
     );
   }
 }
